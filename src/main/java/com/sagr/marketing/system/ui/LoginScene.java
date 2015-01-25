524e561727a9813c6f3b19b3a880edc2b4eac3ad
agrintsov <sasha.grintsov@gmail.com>
2015-01-26 00:44:59 +0200

    JavaFX was included.

---
+++ src/main/java/com/sagr/marketing/system/ui/LoginScene.java
@@ -0,0 +1,14 @@
+package com.sagr.marketing.system.ui;
+
+import javafx.beans.NamedArg;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+/**
+ * @author AGrintsov
+ */
+public class LoginScene extends Scene {
+    public LoginScene(@NamedArg("root") Parent root) {
+        super(root);
+    }
+}
